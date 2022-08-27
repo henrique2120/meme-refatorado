e0b23d33917a00f03766338faae058393ee34669
henrique2120 <riky.souzadias@gmail.com>
2022-08-27 00:08:30 -0300

    pratica-3.js

---
+++ pratica-1.js
@@ -0,0 +1,12 @@
+
+
+
+
+
+
+let contador = 1
+
+while(contador <= 10) {
+console.log(contador)
+contador = contador + 1
+}
---
+++ pratica-2.js
@@ -0,0 +1,19 @@
+
+
+
+
+
+
+
+
+
+
+
+let multiplicador = 9
+let n = 9 //multiplicador de 1 a n
+let i = 1
+
+while (i <= n) {
+  console.log(`${multiplicador} x ${i} = ${multiplicador * i}`)   
+    i++
+} 
